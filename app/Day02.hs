5b662b381341bffaa585ba68b8736dee1f9b1a1a
Thom van der Woude <tbvanderwoude@gmail.com>
2021-12-04 11:38:00 +0100

    Finished day 3

--- app/Day02.hs
+++ app/Day02.hs
@@ -1,4 +1,4 @@
-module Main where
+module Day02 where
 
 import Lib
 
---
+++ app/Day03.hs
@@ -0,0 +1,47 @@
+module Main where
+
+import Data.Bits
+import Lib
+
+repVecAdd :: Num a => [a] -> [[a]] -> [a]
+repVecAdd as [] = as
+repVecAdd as (bs:bss) = repVecAdd (map (uncurry (+)) (zip as bs)) bss 
+
+binMap '0' = 0
+binMap '1' = 1
+binMap c = undefined 
+
+findOxygen :: [[Int]] -> [Int]
+findOxygen (x:[]) = x
+findOxygen [] = undefined
+findOxygen xss = b : findOxygen (map tail (filter (\xs -> b == (head xs)) xss))
+        where l = length xss
+              b = if sum [head xs | xs <- xss]*2 >= l then 1 else 0
+
+findScrubber :: [[Int]] -> [Int]
+findScrubber (x:[]) = x
+findScrubber [] = undefined
+findScrubber xss = b : findScrubber (map tail (filter (\xs -> b == (head xs)) xss))
+        where l = length xss
+              b = if sum [head xs | xs <- xss]*2 < l then 1 else 0
+
+constructInt (x:xs) = x + 2 * (constructInt xs) 
+constructInt [] = 0
+
+binToInt xs = constructInt . reverse $ xs
+
+main :: IO ()
+main = 
+  do inp <- (map (map binMap)) <$> readLines "inputs/input03.txt"
+     let n = length inp :: Int
+     let d = length (head inp) :: Int
+     let counts = repVecAdd (head inp) (tail inp)
+     print (n,d,counts)
+     let binary = map (\x -> if x >= (div n 2) then 1 else 0) counts
+     let epsilonBin = map (\x -> if x >= (div n 2) then 0 else 1) counts
+     let gamma = constructInt (reverse binary) :: Int
+     let epsilon = (complement gamma) `mod` (shiftL 2 (d-1))
+     print (gamma, epsilon, (epsilon * gamma))
+     let oxRating = (binToInt. findOxygen) inp
+     let carbRating = (binToInt . findScrubber) inp
+     print (oxRating,carbRating,(oxRating * carbRating))
