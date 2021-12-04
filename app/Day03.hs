08603db2ff0427d2a5b7681225f5a397288afced
Thom van der Woude <tbvanderwoude@gmail.com>
2021-12-04 11:50:59 +0100

    Simplified vector addition function

--- app/Day03.hs
+++ app/Day03.hs
@@ -3,9 +3,11 @@ module Main where
 import Data.Bits
 import Lib
 
+addLists :: Num a => [a] -> [a] -> [a]
+addLists xs ys = map (uncurry (+)) (zip xs ys)
+
 repVecAdd :: Num a => [a] -> [[a]] -> [a]
-repVecAdd as [] = as
-repVecAdd as (bs:bss) = repVecAdd (map (uncurry (+)) (zip as bs)) bss 
+repVecAdd as bss = foldr addLists as bss 
 
 binMap '0' = 0
 binMap '1' = 1
@@ -16,14 +18,14 @@ findOxygen (x:[]) = x
 findOxygen [] = undefined
 findOxygen xss = b : findOxygen (map tail (filter (\xs -> b == (head xs)) xss))
         where l = length xss
-              b = if sum [head xs | xs <- xss]*2 >= l then 1 else 0
+              b = if sum [head xs | xs <- xss] * 2 >= l then 1 else 0
 
 findScrubber :: [[Int]] -> [Int]
 findScrubber (x:[]) = x
 findScrubber [] = undefined
 findScrubber xss = b : findScrubber (map tail (filter (\xs -> b == (head xs)) xss))
         where l = length xss
-              b = if sum [head xs | xs <- xss]*2 < l then 1 else 0
+              b = if sum [head xs | xs <- xss] * 2 < l then 1 else 0
 
 constructInt (x:xs) = x + 2 * (constructInt xs) 
 constructInt [] = 0
