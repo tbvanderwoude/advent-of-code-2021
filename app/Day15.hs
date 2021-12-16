5cfa327adfe93e4570fd6ea871e2148646de4bbf
Thom van der Woude <tbvanderwoude@gmail.com>
2021-12-16 16:32:18 +0100

    Solved day 15 part 2

--- app/Day15.hs
+++ app/Day15.hs
@@ -13,12 +13,13 @@ type Point = (Int,Int)
 
 type Path = [(Int,Int)]
 type Cost = Int
-data Node = N Point Cost Path
+type Heuristic = Int
+data Node = N Point Cost Heuristic
 
 instance Eq Node where
- (==) (N _ c1 _) (N _ c2 _) = c1 == c2
+ (==) (N _ c1 h1) (N _ c2 h2) = (c1 + h1) == (c2 + h2)
 instance Ord Node where
- compare (N _ c1 _) (N _ c2 _) = compare c1 c2
+ compare (N _ c1 h1) (N _ c2 h2) = compare (c1 + h1) (c2 + h2)
 
 
 getNeighbours :: (Grid a) -> Point -> [Point]
@@ -40,25 +41,41 @@ dijkstra graph nodeFromKey startNode goal = dijkstraIter graph nodeFromKey start
  where startPQ = PQ.singleton startNode :: PQ.MinQueue Node 
        visited = Set.empty :: Set.Set Point
 
+heuristic (x1,y1) (x2,y2) = ((abs (x1 - x2)) + (abs (y1 - y2)))
 
-dijkstraIter graph nodeFromKey pq visited goal = if (point==goal) then c else if (Set.member point visited) then (dijkstraIter graph nodeFromKey reducedPq visited goal) else (dijkstraIter graph nodeFromKey newPq newVisited goal)
- where (N point c path, reducedPq) = PQ.deleteFindMin pq
-       (incr, _, neighbours) = nodeFromKey point :: (Int, Point, [Point])
+dijkstraIter graph nodeFromKey pq visited goal = if (point==goal) then c else if (Set.member point visited) then (dijkstraIter graph nodeFromKey redPq visited goal) else (dijkstraIter graph nodeFromKey newPq newVisited goal)
+ where (N point c h, redPq) = PQ.deleteFindMin pq :: (Node, PQ.MinQueue Node)
+       (_ , _, neighbours) = nodeFromKey point :: (Int, Point, [Point])
        newVisited = Set.insert point visited 
        unvisitedNeighbours = filter (\x -> Set.notMember x newVisited) neighbours 
-       lookups = map nodeFromKey unvisitedNeighbours 
-       newNodes = map (\(c2, n, _) -> N n (c+c2) []) lookups 
-       newPq = foldr PQ.insert reducedPq newNodes
+       newNodes = map (\(c2, n, _)  -> N n (c+c2) (heuristic n goal)) (map nodeFromKey unvisitedNeighbours) :: [Node]
+       newPq = PQ.union redPq (PQ.fromList newNodes)
+
+
+expandGrid grid = concat (buildGrids [(concat (buildRows xs)) | xs <- grid] )
+
+buildGrids :: [[Int]] -> [[[Int]]]
+buildGrids genGrid = [[[wrapper i v | v <- xs] | xs <- genGrid] | i <- [0..4]]
+ 
+wrapper :: Int -> Int -> Int
+wrapper i x = if x + i > 9 then (x+i) - 9 else x + i 
+
+buildRows :: [Int] -> [[Int]]
+buildRows genRow = [map (wrapper i) genRow  | i <- [0..4]]
+
+
+
+solveGrid grid = dijkstra graph nodeFromKey startNode (w-1,h-1)
+ where (graph, nodeFromKey) = constructGridGraph grid
+       startNode = N (0,0) 0 0
+       (w,h) = getDims grid
        
 main :: IO ()
 main = 
   do inp <- readLines "inputs/input15.txt" :: IO [String]
      let grid = map (map digitToInt) inp :: Grid Int     
-     print grid  
-     let (graph, nodeFromKey) = constructGridGraph grid
-     let startNode = N (0,0) 0 []
-     let (w,h) = getDims grid
-     print (w,h)
-     let solution = dijkstra graph nodeFromKey startNode (w-1,h-1)
-     print solution
+     print (solveGrid grid)
+     let expGrid = expandGrid grid
+     print (getDims expGrid)
+     print (solveGrid expGrid)
      return ()
